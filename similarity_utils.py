cd4ee19e0d8021de5f44f1e30c24721126f7c3a9
Alan Hsu <alanhsu.1@gmail.com>
2024-03-18 12:53:59 -0400

    img similarity metrics

---
+++ similarity_utils.py
@@ -0,0 +1,44 @@
+##############################################
+### Image Similarity functions
+##############################################
+
+import numpy as np
+from image_similarity_measures.quality_metrics import rmse, issm, psnr, ssim, uiq
+
+'''
+Installation Notes: 
+pip install pyfftw
+pip install image-similarity-measures[speedups]
+
+How to use:
+(0) import any of the functions here that you want to use
+(1) call vec_to_img on the vector of weights to convert to img
+(2) then call amy of the similarity metrics on two images to get a floating pt value
+
+See img_sim_tests.ipynb for an example
+'''
+
+def vec_to_img(vec):
+    '''
+    vec = 1d numpy array
+    '''
+    img_length = int(len(vec)**0.5)
+    return np.reshape(vec, (img_length, img_length))[..., np.newaxis]
+
+def get_rmse(i1, i2):
+    max_p = max(np.max(i1), np.max(i2))
+    return rmse(i1, i2, max_p)
+
+def get_issm(i1, i2):
+    return issm(i1, i2)
+
+def get_psnr(i1, i2):
+    max_p = max(np.max(i1), np.max(i2))
+    return psnr(i1, i2, max_p)
+
+def get_ssim(i1, i2):
+    max_p = max(np.max(i1), np.max(i2))
+    return ssim(i1, i2, max_p)
+
+def get_uiq(i1, i2):
+    return uiq(i1, i2)
